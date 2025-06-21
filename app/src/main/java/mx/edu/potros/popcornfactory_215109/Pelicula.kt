e7654af419206927b3e7430cfb318f49e294a946
michelle <michellebojorquezsterling@gmail.com>
2025-06-20 17:43:36 -0700

    commit

---
+++ app/src/main/java/mx/edu/potros/popcornfactory_215109/Pelicula.kt
@@ -0,0 +1,7 @@
+package mx.edu.potros.popcornfactory_215109
+
+data class Pelicula (var titulo: String,
+                     var image: Int,
+                     var header: Int,
+                     var sinopsis: String
+)
